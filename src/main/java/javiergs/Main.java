9799c783c2e18a9bcbaee984ca9d03ff2bd31c82
javiergs <3814755+javiergs@users.noreply.github.com>
2024-08-07 03:01:13 -0700

    Update for adding Logtail support

--- src/main/java/javiergs/Main.java
+++ src/main/java/javiergs/Main.java
@@ -11,13 +11,16 @@ import org.slf4j.LoggerFactory;
  */
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Logger logger = LoggerFactory.getLogger(Main.class);
-		logger.trace("This is for tracing");
-		logger.debug("This is for debugging");
-		logger.info("This is just for information");
-		logger.warn("This is warnings");
-		logger.error("This is for errors");
+		logger.trace("1. This is for tracing");
+		logger.debug("2. This is for debugging");
+		logger.info ("3. This is just for information");
+		logger.warn ("4. This is warnings");
+		logger.error("5. This is for errors");
+		
+		Thread.sleep(5000);
 	}
 	
 }
+
